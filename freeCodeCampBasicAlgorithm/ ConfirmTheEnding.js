5bccf810cae0fa87b29bcea2f78d76cf328c8198
luztherose <luzdelarosaramirez@gmail.com>
2020-09-01 14:56:11 -0400

    add pseudo-code

--- freeCodeCampBasicAlgorithm/ ConfirmTheEnding.js
+++ freeCodeCampBasicAlgorithm/ ConfirmTheEnding.js
@@ -4,11 +4,11 @@ Check if a string (first argument, str) ends with the given target string (secon
 This challenge can be solved with the .endsWith() method, which was introduced in ES2015. But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
 */
 
-// find the ending of the str
-// save in a variable
-// compare the value stored in the variable to the second argument, target
-// if the values are equals return true, else false
 function confirmEnding(str, target) {
+    // find the ending of the str
+    // save it in a variable
+    // compare the value stored in the variable to the second argument, target
+    // if the values are equals return true, else false
     let lastOccuranceIndex = str.lastIndexOf(target);
     let newString = str.slice(lastOccuranceIndex);
     if(newString === target) {
