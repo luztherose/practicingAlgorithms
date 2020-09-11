f5bca85d53bf379b5da2676ea9e8fb54a14e409e
luztherose <luzdelarosaramirez@gmail.com>
2020-09-11 11:46:21 -0400

    splice program

---
+++ freeCodeCampBasicAlgorithm/sliceAndSplice.js
@@ -0,0 +1,21 @@
+/*
+    Slice and Splice
+
+    You are given two arrays and an index.
+    Copy each element of the first array into the second array, in order.
+    Begin inserting elements at index n of the second array.
+    Return the resulting array. The input arrays should remain the same after the function runs.
+*/
+
+function frankenSplice(arr1, arr2, n) {
+    // Copy each element of the first array into the second array, in order.
+    // Begin inserting elements at index n of the second array.
+    // Return the resulting array.
+    // The first  and second arrays should remain the same after the function runs.
+    let arrangedArray = [];
+    arrangedArray.push(...arr2);
+    arrangedArray.splice(n, 0, ...arr1 );
+    return arrangedArray;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
