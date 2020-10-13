3fa113ba77ad452665c9b668fe48dbd5803197ae
luztherose <luzdelarosaramirez@gmail.com>
2020-10-13 10:20:29 -0400

    add drop it function

--- freeCodeCampIntermediateAlgorithm/dropIt.js
+++ freeCodeCampIntermediateAlgorithm/dropIt.js
@@ -1,15 +1,26 @@
 /*
+    Drop it
+    Given the array arr, iterate through and remove each element starting from the first element (the 0 index) until the function func returns true when the iterated element is passed through it.
 
+    Then return the rest of the array once the condition is satisfied, otherwise, arr should be returned as an empty array.
 */
 function dropElements(arr, func) {
-    const newArr = arr.filter(item => {
+    let indexValue;
+    // get the index where the iterated element return true
+    const isTrue = arr.some((item, index) => {
+        // console.log(index)
         if (func(item)) {
+            indexValue = index;
             return true;
-        } else {
-            arr.slice(item, 1)
+
         }
-    })
-    return newArr;
+    });
+    // evalute the result to be returned
+    if (isTrue) {
+        return arr.slice(indexValue);
+    } else {
+        return [];
+    }
 }
 
-console.log(dropElements([1, 2, 3], function (n) { return n < 3; }))
+console.log(dropElements([1, 2, 3], function (n) { return n > 0; }));
