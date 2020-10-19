e4200505bdcde0c5b7da74f9538525d6f6caf044
luztherose <luzdelarosaramirez@gmail.com>
2020-10-18 21:34:31 -0400

    binary code

--- freeCodeCampIntermediateAlgorithm/binaryAgents.js
+++ freeCodeCampIntermediateAlgorithm/binaryAgents.js
@@ -5,119 +5,33 @@ The binary string will be space separated.
 
 
 function binaryAgent(str) {
+    const inputStr = str.split(/\s/); // an array of the binary input
+
     // create a data base containing the binary alphabet
-    const binaryAlphabet = [
-        {
-            binaryCode: "01000001",
-            letter: "A"
-        },
-        {
-            binaryCode: "01000010",
-            letter: "B"
-        },
-        {
-            binaryCode: "01000011",
-            letter: "C"
-        },
-        {
-            binaryCode: "01000100",
-            letter: "D"
-        },
-        {
-            binaryCode: "01000101",
-            letter: "E"
-        },
-        {
-            binaryCode: "01000110",
-            letter: "F"
-        },
-        {
-            binaryCode: "01000111",
-            letter: "G"
-        },
-        {
-            binaryCode: "01001000",
-            letter: "H"
-        },
-        {
-            binaryCode: "01001001",
-            letter: "I"
-        },
-        {
-            binaryCode: "01001010",
-            letter: "J"
-        },
-        {
-            binaryCode: "01001011",
-            letter: "K"
-        },
-        {
-            binaryCode: "01001100",
-            letter: "L"
-        },
-        {
-            binaryCode: "01001101",
-            letter: "M"
-        },
-        {
-            binaryCode: "01001110",
-            letter: "N"
-        },
-        {
-            binaryCode: "01001111",
-            letter: "O"
-        },
-        {
-            binaryCode: "01010000",
-            letter: "P"
-        },
-        {
-            binaryCode: "01010001",
-            letter: "Q"
-        },
-        {
-            binaryCode: "01010010",
-            letter: "R"
-        },
-        {
-            binaryCode: "01010011",
-            letter: "S"
-        },
-        {
-            binaryCode: "01010100",
-            letter: "T"
-        },
-        {
-            binaryCode: "01010101",
-            letter: "U"
-        },
-        {
-            binaryCode: "01010110",
-            letter: "V"
-        },
-        {
-            binaryCode: "01010111",
-            letter: "W"
-        },
-        {
-            binaryCode: "01011000",
-            letter: "X"
-        },
-        {
-            binaryCode: "01011001",
-            letter: "Y"
-        },
-        {
-            binaryCode: "01011010",
-            letter: "Z"
-        }
 
-    ]
-    // store the letters 
-    const englishSentence = [];
-    const inputStr = str.split(/\s/);
-    // accessing the binary alphabet database
+    const binaryAlphabet = {
+        "01000001": "A", "01000010": "B", "01000011": "C", "01000100": "D", "01000101": "E", "01000110": "F",
+        "01000111": "G", "01001000": "H", "01001001": "I",
+        "01001010": "J", "01001011": "K", "01001100": "L",
+        "01001101": "M", "01001110": "N", "01001111": "O", "01010000": "P", "01010001": "Q", "01010010": "R", "01010011": "S", "01010100": "T", "01010101": "U",
+        "01010110": "V", "01010111": "W", "01011000": "X",
+        "01011001": "Y", "01011010": "Z", "00100111": "'", "00100000": " ", "00100001": "!", "00111111": "?",
 
-    console.log(englishSentence)
+        "01100001": "a", "01100010": "b", "01100011": "c", "01100100": "d", "01100101": "e", "01100110": "f",
+        "01100111": "g", "01101000": "h", "01101001": "i",
+        "01101010": "j", "01101011": "k", "01101100": "l",
+        "01101101": "m", "01101110": "n", "01101111": "o", "01110000": "p", "01110001": "q", "01110010": "r", "01110011": "s", "01110100": "t", "01110101": "u",
+        "01110110": "v", "01110111": "w", "01111000": "x",
+        "01111001": "y", "01111010": "z"
+    };
+    const letter = [];
+    for (let i = 0; i < inputStr.length; i++) {
+        let binaryCode = inputStr[i];
+        letter.push(binaryAlphabet[binaryCode])
+    }
+
+    return letter.join("");
 }
-binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
+
+
+console.log(binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"))
--- freeCodeCampIntermediateAlgorithm/mapTheDebris.js
+++ freeCodeCampIntermediateAlgorithm/mapTheDebris.js
@@ -8,3 +8,19 @@
     The radius of the earth is 6367.4447 kilometers, and the GM value of earth is 398600.4418 km3s-2.
 
 */
+function orbitalPeriod(arr) {
+    // return a new array that transforms the elements' average altitude into their orbital periods (in seconds).
+    let GM = 398600.4418;
+    let earthRadius = 6367.4447;
+    let radius = Math.round(GM / earthRadius);
+    const avgAlt = 35873.5553;
+    const hoursPerDay = 24;
+    const minutesPerHour = 60;
+    const secondsPerHour = 3600;
+    //average altitude into their orbital periods (in seconds
+
+    console.log(Math.round(((radius * avgAlt) / 60) * 60 / 24))
+    return arr;
+}
+
+orbitalPeriod([{ name: "sputnik", avgAlt: 35873.5553 }]);
